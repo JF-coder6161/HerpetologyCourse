5695949e5bb58266d96ecf22d8ffc223072dd7cd
JF295146895 <18744296161@163.com>
2023-03-07 22:03:18 +0800

    add 模块

---
+++ JS逆向VIP1期/012 Requests应用案例/01 模块.py
@@ -0,0 +1,23 @@
+# 变量 基本数据类型(字符串,布尔类型，整形和浮点型)
+# 运算符
+# 流程控制语句
+# 函数(去重 解耦)
+# 模块
+# 面向对象
+
+import sys
+
+print("sys.path:::",sys.path)
+
+"""
+['F:\\PyCharmProject\\HerpetologyCourse\\JS逆向VIP1期\\012 Requests应用案例', // 优先从启动文件的目录查找 文件 
+ 'F:\\PyCharmProject\\HerpetologyCourse', // pycharm 开发工具 添加的路径
+ 'D:\\software\\pycharm\\PyCharm 2021.3.2\\plugins\\python\\helpers\\pycharm_display', 
+ 'C:\\Users\\18744\\AppData\\Local\\Programs\\Python\\Python310\\python310.zip', 
+   'C:\\Users\\18744\\AppData\\Local\\Programs\\Python\\Python310\\DLLs', 
+   'C:\\Users\\18744\\AppData\\Local\\Programs\\Python\\Python310\\lib', 
+   'C:\\Users\\18744\\AppData\\Local\\Programs\\Python\\Python310', 
+   'F:\\PyCharmProject\\HerpetologyCourse\\venv', 
+   'F:\\PyCharmProject\\HerpetologyCourse\\venv\\lib\\site-packages', 
+   'D:\\software\\pycharm\\PyCharm 2021.3.2\\plugins\\python\\helpers\\pycharm_matplotlib_backend']
+"""
